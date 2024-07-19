717f596adc53f52c45f6d75e240575bd333b8c3b
Aniket Agrawal <ankt7987@gmail.com>
2024-07-20 00:45:01 +0530

    header section

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -7,11 +7,12 @@ import Signup from "../pages/signup";
 import Dashboard from "../pages/dashboard";
 import Signin from "../pages/signin";
 import Projects from "../pages/projects";
-
+import Header from "../src/components/header"
 
 const app = () => {
   return(
     <BrowserRouter>
+        <Header/>
         <Routes>
             <Route path="/" element={<Home/>} ></Route>
             <Route path="/about" element={<About/>} ></Route>
