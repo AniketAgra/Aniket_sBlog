5068048d6ed5aef07b55e5e9020e8f4c5022ca7a
Aniket Agrawal <ankt7987@gmail.com>
2024-09-19 22:23:51 +0530

    database

--- api/index.js
+++ api/index.js
@@ -1,4 +1,15 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv';
+
+dotenv.config();
+
+mongoose.connect(process.env.mongo_url)
+.then(()=> {
+    console.log("Connected To Mongo Successfully");
+}).catch(err => {
+    console.log(err);
+});
 
 const app = express();
 
