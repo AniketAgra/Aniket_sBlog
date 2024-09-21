5cdacec8a0188cfda893aced6aaa62f2f66dda21
Aniket Agrawal <ankt7987@gmail.com>
2024-09-21 20:02:07 +0530

    errorHandler

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,11 +1,12 @@
 import User from '../models/user.model.js'
 import bcryptjs from "bcryptjs";
+import { errorHandler } from '../utils/error.js';
 
-export const signup = async(req,res) => {
+export const signup = async(req,res,next) => {
     const {username, email, password} = req.body;
 
     if(!username || !email || !password || username === '' || email === '' || password === ''){
-        return res.status(400).json({message : " All fields are mandatory"});
+        next(errorHandler(400,'All Fields are mandatory.'))
     }
 
     const hashedPassword = bcryptjs.hashSync(password,10);
@@ -14,6 +15,6 @@ export const signup = async(req,res) => {
         const newUser = await User.create({username, email, password: hashedPassword});
         res.status(200).json({user: newUser._id});
     } catch (error) {
-        res.status(400).json({error : "Invalid Credentials"})
+        next(error);
     }
 }
