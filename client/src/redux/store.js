337036a378ea43e7723a70f08a41da2fe6a7fb1c
Aniket Agrawal <ankt7987@gmail.com>
2024-11-10 22:25:32 +0530

    Redux-Persist

--- client/src/redux/store.js
+++ client/src/redux/store.js
@@ -1,8 +1,25 @@
-import { configureStore } from '@reduxjs/toolkit'
-import userReducer from './user/userSlice'
+import { configureStore, combineReducers } from '@reduxjs/toolkit';
+import userReducer from './user/userSlice';
+import { persistReducer,persistStore } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
+
+const rootReducer = combineReducers({   //combineReducers - combines all reducers into one
+    user: userReducer,
+});
+
+const persistConfig = {       //persistConfig - configuration object for redux-persist
+    key: 'root',
+    storage,
+    version: 1,
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);     //persistedReducer - a higher order reducer that persists the state
 
 export const store = configureStore({
-  reducer: {
-    user: userReducer,
-  },
-})
+    reducer: persistedReducer,
+    middleware: (getDefaultMiddleware) => getDefaultMiddleware({      //getDefaultMiddleware - returns an array of default middlewares
+        serializableCheck: false,
+    }),
+});
+
+export const persistor = persistStore(store);     //persistor - a persisted store
