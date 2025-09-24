0a4d50ff3d40d162dcdfe257b9718c2587b25046
Aniket Agrawal <ankt7987@gmail.com>
2025-09-25 03:00:44 +0530

    Deployment

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
     
     proxy: {
       '/api':{
-      target: 'http://localhost:7000',
+      target: 'https://aniketsblog-lgrb.onrender.com',
       changeOrigin: true,   
       secure: false,
       },
